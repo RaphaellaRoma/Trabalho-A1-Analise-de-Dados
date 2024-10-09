a04e09244fe35bbd5560471952dad18be56b5982
Raphaella Roma <raphysics.oficial@gmail.com>
2024-10-09 15:40:20 -0300

    filtrei alguns dados na função de geração do DataFrame de energia fóssil

--- src/data_cleaner.py
+++ src/data_cleaner.py
@@ -8,8 +8,8 @@ import numpy as np
 import pandas as pd
 
 df = pd.read_csv("..\data\World Energy Consumption.csv")
-print(df.info())
-print(df.head())
+#print(df.info())
+#print(df.head())
 
 columns = df.columns
 
@@ -22,4 +22,18 @@ def renewable_energy_consumption_frame(df):
 
 def GDP_and_fossil_energy_consumption(df):
     # coal, fossil gas, oil, GDP, country
-    pass
+    df_fossil_energy = df[['country','year','gdp','coal_cons_per_capita','fossil_energy_per_capita','gas_energy_per_capita','oil_energy_per_capita']]
+    
+    # Remoção das linhas que não contem dados relativos à verificação
+    no_nulls_rows = df_fossil_energy.dropna(subset=['coal_cons_per_capita','fossil_energy_per_capita','gas_energy_per_capita','oil_energy_per_capita'], how='all')
+    
+    # array de países ou conjuntos agrupados onde o PIB não consta em nenhum dos anos verificados
+    null_gdp_countries = df.groupby('country').filter(lambda x: x['gdp'].isna().all())['country'].unique()
+    
+    # DataFrame sem ps países e agrupados cujo PIB não consta
+    new_df = no_nulls_rows[~no_nulls_rows['country'].isin(null_gdp_countries)]
+    
+    return new_df
+
+
+GDP_and_fossil_energy_frame = GDP_and_fossil_energy_consumption(df)
